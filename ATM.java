1be96f9b7175612fc0221907344b0c5ed7649844
Vaka-vaka <84026149+Vaka-vaka@users.noreply.github.com>
2021-06-17 19:42:56 +0300

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,19 @@
+public class ATM {
+    public int countBanknotes(int sum){
+       int [] numbersBanknotes = new int[]{500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int count = 0;
+        int i = 0;
+        while ( i < numbersBanknotes.length){
+            int justaNumber = numbersBanknotes[i];
+            i++;
+
+            while (sum - justaNumber >=0){
+                    sum -= justaNumber;
+                    count++;
+            }
+        }
+                return count;
+    }
+
+  
+}
