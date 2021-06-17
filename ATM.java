b8f5464701ba64b6079bb8577599a2f2038e0aa4
Vaka-vaka <84026149+Vaka-vaka@users.noreply.github.com>
2021-06-17 19:46:47 +0300

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,6 +1,6 @@
 public class ATM {
     public int countBanknotes(int sum){
-       int [] numbersBanknotes = new int[]{500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int [] numbersBanknotes = new int[]{500, 200, 100, 50, 20, 10, 5, 2, 1};
         int count = 0;
         int i = 0;
         while ( i < numbersBanknotes.length){
@@ -8,12 +8,17 @@ public class ATM {
             i++;
 
             while (sum - justaNumber >=0){
-                    sum -= justaNumber;
-                    count++;
+                sum -= justaNumber;
+                count++;
             }
         }
-                return count;
+        return count;
     }
 
-  
+    public static void main(String[] args) {
+        ATM bank = new ATM();
+
+        //Should be 6 - 500 + 50 + 20 + 5 + 2 + 1
+        System.out.println(bank.countBanknotes(578));
+    }
 }
